9845434870e2fafb515533bb96ca77227d5326bc
Felipe_Brown <felipebrown@gmail.com>
2024-02-17 17:35:50 -0300

    le puse js

---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+
+function mostrarAlerta() {
+    alert("¡El formulario ha sido enviado!");
+  }
+
+  function cambiarEstilo() {
+    var elemento = document.getElementById("titulo-qs");
+    elemento.style.color = "#0dcaf0";
+    elemento.style.fontSize = "3em";
+  }
